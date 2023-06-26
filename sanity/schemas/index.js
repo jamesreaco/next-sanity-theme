20da5d4c851dffeba75086c8cb5f7454332db4a7
James Rea <jamesrea.work@gmail.com>
2023-06-26 11:56:23 +0100

    sanity: added core page builder block schemas to schemaTypes (sanity/schemas/index.js)

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,9 +1,17 @@
 import settings from "./settings";
 import menuItem from "./menuItem";
 import heroBlock from "./blocks/heroBlock";
+import headerBlock from "./blocks/headerBlock";
+import latestPosts from "./blocks/latestPosts";
+import latestCaseStudies from "./blocks/latestCaseStudies";
+import caseStudyArchiveBlock from "./blocks/caseStudyArchiveBlock";
 
 export const schemaTypes = [
   settings,
   menuItem,
-  heroBlock
+  heroBlock,
+  headerBlock,
+  latestPosts,
+  latestCaseStudies,
+  caseStudyArchiveBlock
 ]
