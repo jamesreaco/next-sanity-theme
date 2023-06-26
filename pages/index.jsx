8a539a0e64d2646d18c66f9d56e0e97a35ac0159
James Rea <jamesrea.work@gmail.com>
2023-06-26 13:43:05 +0100

    Configured page builder and data fetching for home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,27 @@
+import { getHomePage } from '@/sanity/utils'
 import Layout from '@/components/global/layout'
+import Block from '@/components/page-builder/block'
 
-export default function Home() {
+export default function Home({ page }) {
+
+  const { pageBuilder } = page
+
   return (
     <Layout>
-      
+      {pageBuilder.map(block => (
+        <Block block={block} />
+      ))} 
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+
+  const page = await getHomePage()
+  
+  return {
+    props: {
+      page: page,
+    }
+  }
 }
