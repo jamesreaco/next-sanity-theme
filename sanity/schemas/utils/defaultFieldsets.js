6ab02993b081b8355833b79c234ad6d2df2aa43f
James Rea <jamesrea.work@gmail.com>
2023-06-26 11:37:01 +0100

    sanity: added defaultFieldsets schema

---
+++ sanity/schemas/utils/defaultFieldsets.js
@@ -0,0 +1,25 @@
+export const defaultFieldsets = [
+  {
+    name: 'seo',
+    title: 'SEO',
+  },
+  {
+    name: 'content',
+    title: 'Content'
+  },
+  {
+    name: 'settings',
+    title: 'Settings'
+  },
+  {
+    name: 'layout',
+    title: 'Layout',
+    description: 'Fine-tune the margin for a perfect layout.',
+    options: {
+      collapsible: true, 
+      collapsed: true, 
+      columns: 2, 
+      modal: {type: 'popover'} 
+    }
+  }
+]
