7d01db48931cb61ef4b963ff2a3acf0d514b585f
James Rea <jamesrea.work@gmail.com>
2023-06-26 12:04:18 +0100

    Added hero-block page builder component

---
+++ components/page-builder/hero-block.jsx
@@ -0,0 +1,31 @@
+import Button from '@/components/global/button'
+import Image from 'next/image'
+
+export default function HeroBlock({ block }) {
+  return (
+    <section className='flex justify-between' style={{ 
+      marginTop: `${block.marginTop * 0.0625}rem`,
+      marginBottom: `${block.marginBottom * 0.0625}rem` 
+    }}>
+      <div className='flex items-start flex-col'>
+        <h1 className='-ml-2 text-[12vw] font-light leading-none'>
+          {block.heading}
+        </h1>
+        <p className='max-w-[30rem] mt-20 mb-12 text-2xl font-light'>
+          {block.paragraph}
+        </p>
+        <Button 
+          text={block.buttonText} 
+          destination={`${block.buttonUrl}`} 
+        />
+      </div>
+      <Image 
+        src="/images/headshot-rounded.png"
+        width={250}
+        height={350}
+        alt='Image of Brooke'
+        className='object-contain'
+      />
+    </section>
+  )
+}
