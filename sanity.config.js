e84beedabcca94bbf127bc70810d3385d45daa04
James Rea <jamesrea.work@gmail.com>
2023-06-26 13:09:58 +0100

    sanity: added schema types to sanity.config.js

--- sanity.config.js
+++ sanity.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
+import { schemaTypes } from '@/sanity/schemas';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
@@ -17,6 +18,9 @@ const config = defineConfig({
     deskTool(),
     visionTool()
   ],
+  schema: { 
+    types: schemaTypes 
+  }
 })
 
 export default config
--- sanity/schemas/blocks/headerBlock.js
+++ sanity/schemas/blocks/headerBlock.js
@@ -1,11 +1,15 @@
 import { DocumentTextIcon } from '@sanity/icons'
 import { defaultLayoutFields } from '../utils/defaultLayoutFields'
+import { defaultFieldsets } from '../utils/defaultFieldsets'
 
 export default {
   name: 'headerBlock',
   title: 'Header Block',
   type: 'object',
   icon: DocumentTextIcon,
+  fieldsets: [
+    ...defaultFieldsets
+  ],
   fields: [
     {
       name: 'heading',
