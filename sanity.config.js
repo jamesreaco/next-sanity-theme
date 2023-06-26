51f2de6472c121b2584ecfb1e51fdd0139594875
James Rea <jamesrea.work@gmail.com>
2023-06-26 13:19:18 +0100

    sanity: set up custom desk structure

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from '@/sanity/schemas';
+import { structure } from './sanity/structure';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
@@ -15,7 +16,9 @@ const config = defineConfig({
   apiVersion: apiVersion,
   basePath: '/studio',
   plugins: [
-    deskTool(),
+    deskTool({
+      structure
+    }),
     visionTool()
   ],
   schema: { 
---
+++ sanity/structure.js
@@ -0,0 +1,47 @@
+import { CogIcon } from '@sanity/icons'
+
+export const structure = (S, context) =>
+  S.list()
+    .title('Content')
+    .items([
+      
+      S.listItem()
+        .title('Site Settings')
+        .icon(CogIcon)
+        .child(
+          S.document()
+          .id('settings')
+          .schemaType('settings')
+          .documentId('settings')
+          .title('Site Settings')
+        ),
+
+      S.divider(),
+
+      S.listItem()
+        .title('Core Pages')
+        .child(
+          S.list()
+            .title('Core Pages')
+            .items([
+              S.listItem()
+                .title('Home')
+                .child(S.document().schemaType('homePage').documentId('homePage').title('Home Page')),
+            ])
+        ),
+      
+      S.divider(),
+
+      ...S.documentTypeListItems().filter(
+        (listItem) =>
+          ![
+            'settings',
+            'homePage',
+          ].includes(listItem.getId())
+      )
+
+    ])
+    
+  
+    
+
