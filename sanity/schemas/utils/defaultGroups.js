685fc122ad12144509e09f4760cbc10fa4ca8008
James Rea <jamesrea.work@gmail.com>
2023-06-26 11:38:57 +0100

    sanity: added defaultGroups schema

---
+++ sanity/schemas/utils/defaultGroups.js
@@ -0,0 +1,18 @@
+export const defaultGroups = [
+  {
+    name: 'content',
+    title: 'Content'
+  },
+  {
+    name: 'seo',
+    title: 'SEO'
+  },
+  {
+    name: 'settings',
+    title: 'Page Settings'
+  },
+  {
+    name: 'layout',
+    title: 'Layout'
+  }
+]
