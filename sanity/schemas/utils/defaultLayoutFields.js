a1b7d25fcc4ec6c3d7020d3f212d841224c257ac
James Rea <jamesrea.work@gmail.com>
2023-06-26 11:39:52 +0100

    sanity: added defaultLayoutFields schema

---
+++ sanity/schemas/utils/defaultLayoutFields.js
@@ -0,0 +1,38 @@
+export const layoutFields = [
+  {
+    name: 'marginTop',
+    title: 'Margin Top - Desktop',
+    type: 'string',
+    fieldset: 'layout'
+  },
+  {
+    name: 'marginBottom',
+    title: 'Margin Bottom - Desktop',
+    type: 'string',
+    fieldset: 'layout'
+  },
+  {
+    name: 'marginTopTablet',
+    title: 'Margin Top - Tablet',
+    type: 'string',
+    fieldset: 'layout'
+  },
+  {
+    name: 'marginBottomTablet',
+    title: 'Margin Bottom - Tablet',
+    type: 'string',
+    fieldset: 'layout'
+  },
+  {
+    name: 'marginTopMobile',
+    title: 'Margin Top - Mobile',
+    type: 'string',
+    fieldset: 'layout'
+  },
+  {
+    name: 'marginBottomMobile',
+    title: 'Margin Bottom - Mobile',
+    type: 'string',
+    fieldset: 'layout'
+  },
+]
