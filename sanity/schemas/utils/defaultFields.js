87317a8cf702022dedbd62a7af80d20115863666
James Rea <jamesrea.work@gmail.com>
2023-06-26 11:38:09 +0100

    sanity: added defaultFields schema

---
+++ sanity/schemas/utils/defaultFields.js
@@ -0,0 +1,38 @@
+export const defaultFields = [
+  {
+    name: 'heading',
+    title: 'Heading',
+    type: 'string',
+    group: 'content',
+    fieldset: 'content'
+  },
+  {
+    name: 'tagline',
+    title: 'Tagline',
+    type: 'string',
+    group: 'content',
+    fieldset: 'content'
+  },
+  {
+    name: 'metaTitle',
+    title: 'Meta Title',
+    type: 'string',
+    group: 'seo',
+    fieldset: 'seo'
+  },
+  {
+    name: 'metaDescription',
+    title: 'Meta Description',
+    type: 'string',
+    group: 'seo',
+    fieldset: 'seo'
+  },
+  {
+    name: 'metaKeywords',
+    title: 'Meta Keywords',
+    description: 'Add keywords seperated by a comma (apple, banana, pear).',
+    type: 'string',
+    group: 'seo',
+    fieldset: 'seo'
+  },
+]
