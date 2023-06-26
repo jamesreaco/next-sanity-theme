a75085323ef3757f51082ec9f9b51a9ff9d28ded
James Rea <jamesrea.work@gmail.com>
2023-06-26 13:29:05 +0100

    Added blog-card component

---
+++ components/shared/blog-card.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function BlogCard({ post }) {
+  return (
+    <article className='pb-12 flex items-center justify-between border-b border-black cursor-pointer group'>
+      <h4 className='text-[4vw] font-normal'>
+        {post.title}
+      </h4>
+      <Image
+        src="/images/link-icon.png"
+        width={40}
+        height={40}
+        className='invert-[100%] transition group-hover:rotate-90'
+      />
+    </article>
+  )
+}
