a65199e4c03664f1833f13a017e6ba96faff4136
James Rea <jamesrea.work@gmail.com>
2023-06-26 11:28:55 +0100

    sanity: added menuItem schema

---
+++ sanity/schemas/menuItem.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'menuItem',
+  title: 'Menu Items',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'string',
+    },
+  ]
+}
