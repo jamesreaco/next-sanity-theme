6d082a57d106b6f6f82e3a85d8fd5adb554cbec9
James Rea <jamesrea.work@gmail.com>
2023-06-27 10:27:54 +0100

    Added category pill to blog-card and blog-row

--- components/shared/blog-card.jsx
+++ components/shared/blog-card.jsx
@@ -2,14 +2,17 @@ import Image from 'next/image'
 
 export default function BlogCard({ post }) {
   return (
-    <article className='pb-8 border-b border-black cursor-pointer group'>
+    <article className='flex flex-col items-start pb-8 border-b border-black cursor-pointer group'>
       <Image 
         src=""
         width={600}
         height={300}
         className='bg-black rounded-lg'
       />
-      <div className='mt-6 flex items-end justify-between'>
+      <h5 className='mt-5 py-0.5 px-2.5 border-solid border rounded-full'>
+        {post.category.title}
+      </h5>
+      <div className='mt-3 flex items-end justify-between'>
         <h4 className='text-[2.8vw] pr-12 font-normal'>
           {post.title}
         </h4>
--- components/shared/blog-row.jsx
+++ components/shared/blog-row.jsx
@@ -3,7 +3,10 @@ import Image from 'next/image'
 export default function BlogRow({ post }) {
   return (
     <article className='pb-8 flex items-center justify-between border-b border-black cursor-pointer group'>
-      <h4 className='text-[4vw] font-normal'>
+      <h5 className='mt-3 py-0.5 px-2.5 border-solid bg-black text-white border rounded-full'>
+        {post.category.title}
+      </h5>
+      <h4 className='mr-auto ml-6 text-[4vw] font-normal'>
         {post.title}
       </h4>
       <Image
