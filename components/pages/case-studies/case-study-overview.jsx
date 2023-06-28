483236b717aaf4d9967d9834eedf5bc5cf127228
James Rea <jamesrea.work@gmail.com>
2023-06-28 15:03:29 +0100

    Increased case-study-overview top/bottom margin

--- components/pages/case-studies/case-study-overview.jsx
+++ components/pages/case-studies/case-study-overview.jsx
@@ -3,7 +3,7 @@ import Content from '@/components/shared/content';
 export default function CaseStudyOverview({ overview }) {
   return (
     <section>
-      <div className='max-w-lg mx-auto my-20'>
+      <div className='max-w-lg mx-auto my-36'>
         <h2 className='mb-8 text-6xl font-extralight'>
           Project Overview
         </h2>
