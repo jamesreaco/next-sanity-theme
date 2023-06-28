d2b6be7cd2bdeb696249295a309cd406ac55dd19
James Rea <jamesrea.work@gmail.com>
2023-06-28 15:38:55 +0100

    Added content component to post-content component

--- components/pages/blog/post-content.jsx
+++ components/pages/blog/post-content.jsx
@@ -1,9 +1,9 @@
-import { PortableText } from '@portabletext/react';
+import Content from '@/components/shared/content';
 
 export default function PostContent({ content }) {
   return (
-    <div className='pt-10 mx-10 pb-14 text-lg text-gray-700 border-t blog-content'>
-      <PortableText value={content} />
+    <div className='max-w-xl mx-auto pt-8 pb-20 text-lg text-gray-700 border-t blog-content'>
+      <Content data={content} />
     </div>
   )
 }
--- components/pages/blog/post-header.jsx
+++ components/pages/blog/post-header.jsx
@@ -8,7 +8,7 @@ export default function PostHeader({
   title
 }) {
   return (
-    <header className='grid mt-8'>
+    <header className='mt-8'>
       <Image 
         src={image}
         width={2000}
@@ -16,8 +16,8 @@ export default function PostHeader({
         alt='Thumbnail Image'
         className='rounded-2xl'
       />
-      <div className='p-10 flex flex-col items-start justify-start'>
-        <div className='mt-5 flex items-center '>
+      <div className='max-w-xl mx-auto my-10'>
+        <div className='mt-20 flex items-center'>
           <Tag text={category} />
           <div className='ml-3 text-gray-500 font-light tracking-wide'>
             {readTime} Minute Read
@@ -30,3 +30,5 @@ export default function PostHeader({
     </header>
   )
 }
+
+
