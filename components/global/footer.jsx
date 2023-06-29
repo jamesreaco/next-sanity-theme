7fb5e3c49b3df00b103b2ad43139a900cec819ab
James Rea <jamesrea.work@gmail.com>
2023-06-29 10:16:33 +0100

    Optimised footer styles for mobile and tablet devices

--- components/global/footer.jsx
+++ components/global/footer.jsx
@@ -12,14 +12,14 @@ export default function Footer({
   return (
     <footer className='bg-white border-t'>
       <div className='max-w-7xl mx-auto p-12 pb-6 px-6 md:px-12'>
-        <div className='flex'>
-          <div className='mr-auto'>
+        <div className='flex flex-col md:flex-row'>
+          <div className='w-full md:w-fit mr-auto pb-10 md:pb-0 border-b md:border-b-0 border-gray-100'>
             <Logo text={logoText} />
             <p className='mt-4'>
               {footerTagline}
             </p>
           </div>
-          <div className='grid grid-cols-2 gap-24'>
+          <div className='mt-10 md:mt-0 pb-6 md:pb-0 grid grid-cols-2 gap-24 border-b md:border-b-0 border-gray-100'>
             <div>
               <h5 className='mb-8 font-medium'>
                 Quick Links
@@ -50,10 +50,13 @@ export default function Footer({
             </div>
           </div>
         </div>
-        <div className='mt-28 flex justify-between'>
-          <p className='font-light text-sm'>
-            {footerCopyright}
-            <span>
+        <div className='mt-10 md:mt-28 flex flex-col md:flex-row justify-between'>
+          <div className='flex flex-col md:flex-row'>
+            <p className='font-light text-sm'>
+              {footerCopyright}
+            </p>
+            <span className='mt-1 md:mt-0 ml-0 md:ml-1 font-light text-sm'>
+              A free and open source theme by
               <a 
                 href="https://twitter.com/jamesreaco" 
                 className='font-medium text-blue-800'
@@ -61,13 +64,13 @@ export default function Footer({
               &nbsp;James Rea
               </a>
             </span>
-          </p>
+          </div>
           <div className='flex items-center gap-1'>
             {footerLegalLinks?.map((item) => (
               <div className='flex items-center gap-1 group'>
                 <Link 
                   href={item.link}
-                  className='text-sm'
+                  className='mt-1 md:mt-0 text-sm'
                 >
                   {item.title}
                 </Link>
