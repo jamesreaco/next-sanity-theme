e621f991a9ddf0ff619e3338977c6b31ccde5a45
James Rea <jamesrea.work@gmail.com>
2023-07-03 09:34:02 +0100

    Optimised home-case-studies styles for mobile and tablet devices

--- components/pages/home/home-case-studies.jsx
+++ components/pages/home/home-case-studies.jsx
@@ -20,14 +20,14 @@ export default function HomeCaseStudies() {
   }
 
   return (
-    <section className='relative mt-16 mb-28 pt-64'>
+    <section className='relative mt-16 mb-28 pt-28 md:pt-64'>
       <motion.div 
-        className='-left-[40rem] top-0 absolute text-[10rem] font-extralight whitespace-nowrap' 
+        className='-left-[40rem] top-0 absolute text-[4rem] md:text-[10rem] font-extralight whitespace-nowrap' 
         style={{ x }}
       >
         Case Studies ✦ Case Studies ✦ Case Studies ✦ Case Studies ✦ Case Studies ✦ Case Studies
       </motion.div>
-      <div className='flex gap-4'>
+      <div className='flex flex-col md:flex-row gap-6 md:gap-4'>
         {caseStudies.map((item) => (
           <CaseStudyCard caseStudy={item} />
         ))}
