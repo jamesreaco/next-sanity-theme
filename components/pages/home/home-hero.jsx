cafca6943fc05452db640cd44a2f5e1e16653632
James Rea <jamesrea.work@gmail.com>
2023-07-03 09:43:13 +0100

    Optimised home-hero styles for mobile and tablet devices

--- components/pages/home/home-hero.jsx
+++ components/pages/home/home-hero.jsx
@@ -9,12 +9,12 @@ export default function HomeHero({
   btnDestination
 }) {
   return (
-    <section className='mt-20 flex flex-col-reverse md:flex-row justify-between'>
+    <section className='mt:0 md:mt-20 flex flex-col-reverse md:flex-row justify-between'>
       <div className='flex items-start flex-col'>
         <h1 className='-ml-1 md:-ml-2 text-[12vw] font-light leading-none'>
           {heading}
         </h1>
-        <p className='max-w-[30rem] mt-10 md:mt-20 mb-12 text-[1.2rem] md:text-2xl font-light'>
+        <p className='max-w-[30rem] mt-6 md:mt-20 mb-8 md:mb-12 text-[1rem] md:text-2xl font-light'>
           {paragraph}
         </p>
         <Button 
@@ -28,7 +28,7 @@ export default function HomeHero({
         width={250}
         height={350}
         alt='Image of Brooke'
-        className='w-[10rem] md:w-[14rem] mb-10 md:mb-0 mt-20 md:mt-0 object-contain'
+        className='w-[6rem] md:w-[12rem] mb-10 md:mb-0 mt-10 md:mt-20 object-contain'
       />
     </section>
   )
--- components/shared/button.jsx
+++ components/shared/button.jsx
@@ -5,7 +5,7 @@ export default function Button({ text, destination, variant }) {
   return (
     <Link 
       href={`${destination}`} 
-      className='flex items-center justify-between py-3 px-5 text-lg tracking-widest rounded-lg cursor-pointer'
+      className='flex items-center justify-between py-2 md:py-3 px-4 md:px-5 text-base md:text-lg tracking-widest rounded-lg cursor-pointer'
       style={{ 
         backgroundColor: variant === 'dark' ? `#000` : `#f6f6f2`,
         color: variant === 'dark' ? `#fff` : `#000`,
@@ -18,7 +18,7 @@ export default function Button({ text, destination, variant }) {
         width={12}
         height={12}
         alt="Link icon"
-        className='ml-12'
+        className='ml-8 md:ml-12'
         style={{
           filter: variant === 'dark' ? `invert(0)` : `invert(0.8)`,
         }}
